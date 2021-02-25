b6a55feb0d8c2500696bdd400e75fad693cef7bf
st0n3 <ssst0n3@gmail.com>
2021-02-25 15:53:49 +0800

    ubuntu-21.04_docker-ce-20.10.3_docker-ce-cli-20.10.3_containerd.io-1.4.3_runc-1.0.0-rc92

--- start_vm.sh
+++ start_vm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 qemu-system-x86_64 \
 -m 1024 \
--hda ubuntu-20.04-server-cloudimg-amd64.img \
+-hda ubuntu-server-cloudimg.img \
 -net nic -net user,hostfwd=tcp::22-:22 \
 -smp 2 \
 -m 2048M \
