2d28def9d2ffaaae327a6e9d43a7825f1cdc9feb
Lei Wang <ssst0n3@gmail.com>
2022-11-26 10:33:43 +0800

    replace 1.6.0 to 1.5.9

--- init.sh
+++ init.sh
@@ -16,10 +16,11 @@ EOF
 sysctl --system
 apt-get update
 apt-get install -y libseccomp2
-export VERSION=1.6.0
+export VERSION=1.5.9
 wget https://github.com/containerd/containerd/releases/download/v${VERSION}/cri-containerd-cni-${VERSION}-linux-amd64.tar.gz
 tar --no-overwrite-dir -C / -xzf cri-containerd-cni-${VERSION}-linux-amd64.tar.gz
 systemctl daemon-reload
+systemctl enable containerd
 systemctl start containerd
 mkdir -p /etc/containerd
 containerd config default | sudo tee /etc/containerd/config.toml
@@ -35,8 +36,12 @@ kubeadm init --pod-network-cidr=192.168.0.0/16 --kubernetes-version=1.23.4
 mkdir -p /root/.kube
 cp /etc/kubernetes/admin.conf /root/.kube/config
 export KUBECONFIG=/etc/kubernetes/admin.conf
-kubectl create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml
-kubectl create -f https://docs.projectcalico.org/manifests/custom-resources.yaml
+# kubectl create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml
+# kubectl create -f https://docs.projectcalico.org/manifests/custom-resources.yaml
+# kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.22.1/manifests/tigera-operator.yaml
+# kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.22.1/manifests/custom-resources.yaml
+kubectl create -f https://projectcalico.docs.tigera.io/archive/v3.22/manifests/tigera-operator.yaml
+kubectl create -f https://projectcalico.docs.tigera.io/archive/v3.22/manifests/custom-resources.yaml
 kubectl taint nodes --all node-role.kubernetes.io/master-
 while true
 do
