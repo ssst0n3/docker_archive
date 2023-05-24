02a8db6ecb803de95f72d68650dbf03cf1103f5d
LEI WANG <ssst0n3@gmail.com>
2023-05-24 18:04:52 +0800

    invalid chars
    Signed-off-by: LEI WANG <ssst0n3@gmail.com>

--- start_vm.sh
+++ start_vm.sh
@@ -3,7 +3,7 @@ qemu-system-x86_64 \
 -m 2560M \
 -hda ubuntu-server-cloudimg.img \
 -smp 2 \
--nographic \ 
+-nographic \
 -net nic -net user,hostfwd=tcp::22-:22,hostfwd=tcp::2345-:2345 \
 -cpu host \
 -enable-kvm $*
