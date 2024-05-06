388a5135d616b4603010a176062c1275548cc915
LEI WANG <ssst0n3@gmail.com>
2024-05-06 15:12:15 +0800

    install xterm
    Signed-off-by: LEI WANG <ssst0n3@gmail.com>

--- ubuntu/22.04/build.sh
+++ ubuntu/22.04/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-alias d2vm="docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock --privileged -v \$PWD:/d2vm -w /d2vm linkacloud/d2vm:latest"
+function d2vm() {
+	docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock --privileged -v $PWD:/d2vm -w /d2vm linkacloud/d2vm:latest $*
+}
 d2vm build -o vm.qcow2 . -v
