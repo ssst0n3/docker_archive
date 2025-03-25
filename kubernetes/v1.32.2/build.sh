1e8bb1e1a2733abd993f5e74baeb1052be39bf6a
LEI WANG <ssst0n3@gmail.com>
2025-03-25 22:09:03 +0800

    update
    Signed-off-by: LEI WANG <ssst0n3@gmail.com>

--- kubernetes/v1.32.2/build.sh
+++ kubernetes/v1.32.2/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker buildx build --build-arg BUILDKIT_SANDBOX_HOSTNAME=kind-control-plane --progress=plain --builder insecure-builder --allow security.insecure --load -t $1 .
+docker buildx build --network=docker_archive --build-arg BUILDKIT_SANDBOX_HOSTNAME=kind-control-plane --progress=plain --builder insecure-builder --allow security.insecure --load -t $1 .
