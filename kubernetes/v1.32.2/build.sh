f5c819598673211ea62ed800961100f002d2fe33
LEI WANG <ssst0n3@gmail.com>
2025-03-27 13:54:50 +0800

    complete
    Signed-off-by: LEI WANG <ssst0n3@gmail.com>

--- kubernetes/v1.32.2/build.sh
+++ kubernetes/v1.32.2/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# docker buildx rm insecure-builder
-# docker buildx create --name insecure-builder --buildkitd-flags "--allow-insecure-entitlement security.insecure"
-docker buildx build --build-arg BUILDKIT_SANDBOX_HOSTNAME=kind-control-plane --progress=plain --builder insecure-builder --allow security.insecure --load -t $1 .
+# https://docs.docker.com/build/builders/drivers/docker-container/#custom-network
+docker network create --subnet 10.0.2.0/24 --ip-range 10.0.2.16/28 --gateway 10.0.2.1 docker-archive-bridge
+docker buildx create --driver-opt "network=docker-archive-bridge" --name docker-archive-builder --buildkitd-flags "--allow-insecure-entitlement security.insecure"
+docker buildx build --build-arg BUILDKIT_SANDBOX_HOSTNAME=kind-control-plane --progress=plain --builder docker-archive-builder --allow security.insecure --load -t $1 .
