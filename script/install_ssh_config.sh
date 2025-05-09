412c97a1cbb3f2884b5b8a55dea907540807f348
Lei Wang <ssst0n3@gmail.com>
2025-05-09 23:41:19 +0800

    insert instead of append, to prevent the ssh config won't be parsed if the config is not empty

--- script/install_ssh_config.sh
+++ script/install_ssh_config.sh
@@ -3,4 +3,4 @@ PROJECT_DIR="$(dirname "$(dirname "$(realpath "$0")")")"
 
 mkdir -p ~/.ssh/keys
 cp ssh_config/docker_archive ~/.ssh/keys/
-echo "include ${PROJECT_DIR}/ssh_config/config" >> ~/.ssh/config
+sed -i '1i include '"${PROJECT_DIR}"'/ssh_config/config' ~/.ssh/config
