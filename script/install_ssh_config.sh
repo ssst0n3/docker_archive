77405a3de42b7869d7d05c13525e3d60cff2fcae
LEI WANG <ssst0n3@gmail.com>
2025-07-11 11:25:06 +0800

    fix cp No such file or directory bug
    Signed-off-by: LEI WANG <ssst0n3@gmail.com>

--- script/install_ssh_config.sh
+++ script/install_ssh_config.sh
@@ -2,6 +2,6 @@
 PROJECT_DIR="$(dirname "$(dirname "$(realpath "$0")")")"
 
 mkdir -p ~/.ssh/keys
-cp ssh_config/docker_archive ~/.ssh/keys/
-cp ssh_config/docker_archive_ecdsa-sha2-nistp256 ~/.ssh/keys/
+cp ${PROJECT_DIR}/ssh_config/docker_archive ~/.ssh/keys/
+cp ${PROJECT_DIR}/ssh_config/docker_archive_ecdsa-sha2-nistp256 ~/.ssh/keys/
 sed -i '1i include '"${PROJECT_DIR}"'/ssh_config/config' ~/.ssh/config
