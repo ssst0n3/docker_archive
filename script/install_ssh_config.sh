1cc6ef201dceeeb23a59e95403fec6c4e4515305
LEI WANG <ssst0n3@gmail.com>
2025-09-16 11:42:22 +0800

    [fix] fix a bug when ~/.ssh/config is empty
    Signed-off-by: LEI WANG <ssst0n3@gmail.com>

--- script/install_ssh_config.sh
+++ script/install_ssh_config.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-PROJECT_DIR="$(dirname "$(dirname "$(realpath "$0")")")"
+set -e
 
-mkdir -p ~/.ssh/keys
-cp ${PROJECT_DIR}/ssh_config/docker_archive ~/.ssh/keys/
-cp ${PROJECT_DIR}/ssh_config/docker_archive_ecdsa-sha2-nistp256 ~/.ssh/keys/
-sed -i '1i include '"${PROJECT_DIR}"'/ssh_config/config' ~/.ssh/config
+PROJECT_DIR="$(dirname "$(dirname "$(realpath "$0")")")"
+SSH_DIR=~/.ssh
+KEYS_DIR=${SSH_DIR}/keys
+CONFIG_FILE=${SSH_DIR}/config
+INCLUDE_CONFIG_PATH="${PROJECT_DIR}/ssh_config/config"
+INCLUDE_LINE="Include ${INCLUDE_CONFIG_PATH}"
+
+mkdir -p "${KEYS_DIR}"
+cp "${PROJECT_DIR}/ssh_config/docker_archive" "${KEYS_DIR}/"
+cp "${PROJECT_DIR}/ssh_config/docker_archive_ecdsa-sha2-nistp256" "${KEYS_DIR}/"
+
+# Ensure the config file exists
+touch "${CONFIG_FILE}"
+
+# Prepend the include line if it's not already there
+if ! grep -qF -- "${INCLUDE_LINE}" "${CONFIG_FILE}"; then
+  TMP_FILE=$(mktemp)
+  echo "${INCLUDE_LINE}" > "${TMP_FILE}"
+  cat "${CONFIG_FILE}" >> "${TMP_FILE}"
+  mv "${TMP_FILE}" "${CONFIG_FILE}"
+fi
