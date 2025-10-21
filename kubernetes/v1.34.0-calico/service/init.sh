aa4284f99bd48101b711b117406aabf9fbda00fa
LEI WANG <ssst0n3@gmail.com>
2025-10-21 16:59:14 +0800

    kubernetes-v1.34.0-calico
    Signed-off-by: LEI WANG <ssst0n3@gmail.com>

---
+++ kubernetes/v1.34.0-calico/service/init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# wait pods ready
+until kubectl wait --for=condition=Ready pod --all -A --field-selector=metadata.namespace=kube-system -l "k8s-app!=kube-dns" --timeout=5s; do sleep 1; done >>/dev/kmsg 2>&1
+
+# install network addon
+# https://docs.tigera.io/calico/3.30/getting-started/kubernetes/requirements#kubernetes-requirements
+helm repo add projectcalico https://docs.tigera.io/calico/charts >>/dev/kmsg 2>&1
+kubectl create namespace tigera-operator >>/dev/kmsg 2>&1
+helm install calico projectcalico/tigera-operator --version v3.30.4 --namespace tigera-operator >>/dev/kmsg 2>&1
+
+# wait pods ready
+echo "Waiting for all pods ready" >> /dev/kmsg
+until kubectl wait --for=condition=Ready pod --all -A --timeout=5s; do sleep 1; done >>/dev/kmsg 2>&1
+
+# remove unused containers
+crictl rm $(crictl ps -a -q) >>/dev/kmsg 2>&1
+
+# prevent data lost
+sync
+
+# gracefully exit
+systemctl exit 0
