89afed89e0abbe6d99bb6f554b42a0df380bc72a
LEI WANG <ssst0n3@gmail.com>
2025-10-23 00:00:52 +0800

    fix(build): update buildx prune command to target exec.cachemount type
    Replaced the old prune filter for kubernetes-v1.18.2-snapshots with a more specific
    filter targeting exec.cachemount. Ensures cleanup applies correctly with
    the designated builder (docker-archive-builder).
    
    Signed-off-by: LEI WANG <ssst0n3@gmail.com>

--- kubernetes/v1.18.2-calico/build.sh
+++ kubernetes/v1.18.2-calico/build.sh
@@ -2,5 +2,5 @@
 # https://docs.docker.com/build/builders/drivers/docker-container/#custom-network
 docker network create --subnet 10.0.2.0/24 --ip-range 10.0.2.16/28 --gateway 10.0.2.1 docker-archive-bridge
 docker buildx create --driver-opt "network=docker-archive-bridge" --name docker-archive-builder --buildkitd-flags "--allow-insecure-entitlement security.insecure"
-docker buildx prune --filter name=kubernetes-v1.18.2-snapshots -f
+docker buildx --builder docker-archive-builder prune --filter type=exec.cachemount -f
 docker buildx build --build-arg BUILDKIT_SANDBOX_HOSTNAME=kubernetes-1-18-2 --progress=plain --builder docker-archive-builder --allow security.insecure --load -t $1 .
