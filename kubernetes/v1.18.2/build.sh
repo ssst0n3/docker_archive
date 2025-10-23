18e9a99016aa026b9295d16451ca9bc802b3f151
LEI WANG <ssst0n3@gmail.com>
2025-10-23 16:19:06 +0800

    fix(build.sh): use variable paths for nf_conntrack parameters
    Define PARAM_PATH and SYSCTL_PATH variables to avoid hard-coded paths when setting
    nf_conntrack hash size. Improves script maintainability and consistency with
    system parameter management.
    
    Signed-off-by: LEI WANG <ssst0n3@gmail.com>

--- kubernetes/v1.18.2/build.sh
+++ kubernetes/v1.18.2/build.sh
@@ -3,9 +3,11 @@ set -x
 # fix kube-proxy `write /sys/module/nf_conntrack/parameters/hashsize: operation not supported`
 # https://blog.michali.net/2017/08/09/ipv6-support-for-docker-in-docker/
 # https://github.com/kubernetes/kubernetes/blob/v1.18.2/cmd/kube-proxy/app/conntrack.go#L60-L66
+PARAM_PATH="/sys/module/nf_conntrack/parameters/hashsize"
+SYSCTL_PATH="/proc/sys/net/netfilter/nf_conntrack_max"
 MAX=$(< "$SYSCTL_PATH")
 HASH_SIZE=$(( MAX / 4 ))
-echo ${HASH_SIZE} > /sys/module/nf_conntrack/parameters/hashsize
+echo "$HASH_SIZE" > "$PARAM_PATH"
 
 # https://docs.docker.com/build/builders/drivers/docker-container/#custom-network
 docker network create --subnet 10.0.2.0/24 --ip-range 10.0.2.16/28 --gateway 10.0.2.1 docker-archive-bridge
