5b38d937f606b660314de7e6e1557a18b1e94510
st0n3 <st0n3@debian.home>
2025-11-23 23:08:27 +0800

    kubernetes-v1.26.0

---
+++ kubernetes/v1.26.0/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x
+
+# https://docs.docker.com/build/builders/drivers/docker-container/#custom-network
+docker network create --subnet 10.0.2.0/24 --ip-range 10.0.2.16/28 --gateway 10.0.2.1 docker-archive-bridge
+docker buildx create --driver-opt "network=docker-archive-bridge" --name docker-archive-builder \
+    --buildkitd-flags "--allow-insecure-entitlement security.insecure"
+
+docker buildx --builder docker-archive-builder prune --filter type=exec.cachemount -f || true
+mkdir -p modules
+cp /lib/modules/$(uname -r) modules/$(uname -r) -r
+docker buildx build \
+    --build-arg CACHE_BUST=$(date +%s) \
+    --build-arg BUILDKIT_SANDBOX_HOSTNAME=kubernetes-1-26-0 \
+    --progress=plain --builder docker-archive-builder \
+    --allow security.insecure \
+    --load -t $1 .
+rm modules -r
